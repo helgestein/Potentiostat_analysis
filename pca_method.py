02a71cc4b6628640c224fa9eab32e02a7107d372
Fuzhan R <fuzhanrahmanian@gmail.com>
2020-03-17 14:15:25 +0100

    Update pca_method.py

--- pca_method.py
+++ pca_method.py
@@ -26,8 +26,8 @@ X_pca_test = pca.fit_transform(data['current'][data['test_ix']])[0:6]
 pca = PCA(100)
 X_pca_val = pca.fit_transform(data['current'][data['val']])[0:6]
 
-scan_rate = np.reshape(data['v'][0:6], (-1, 1))
-concentration = np.reshape(data['c'][0:6], (-1, 1))
+scan_rate = np.reshape(data['v'], (-1, 1))
+concentration = np.reshape(data['c'], (-1, 1))
 
 m = np.append(X_pca_train, scan_rate, axis=1)
 m.shape

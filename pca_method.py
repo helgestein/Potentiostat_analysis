bbdbce39d25dc4e8879d86410163de13de272809
Fuzhi_Surface <fuzhanrahmanian@gmail.com>
2020-03-17 17:38:31 +0100

    Update pca_method.py

--- pca_method.py
+++ pca_method.py
@@ -14,30 +14,59 @@ def do_pca(n_components, data):
     variance_score = sum(pca.explained_variance_ratio_[0:6])
     return pca, X_pca, variance_score
 
-pca = PCA(100)
-X_pca_train = pca.fit_transform(data['current'][data['train_ix']])[0:6]
-var = pca.explained_variance_ratio_
+#train data
+pca = PCA(n_components= 6)
+X_pca_train = pca.fit_transform(data['current'][data['train_ix']])
+#var = pca.explained_variance_ratio_
 sum(pca.explained_variance_ratio_[0:6])
 
-pca = PCA(100)
-X_pca_test = pca.fit_transform(data['current'][data['test_ix']])[0:6]
+scan_rate_train = np.reshape(data['v'][data['train_ix']], (-1, 1))
 
+concentration_train = np.reshape(data['c'][data['train_ix']], (-1,1))
 
-pca = PCA(100)
-X_pca_val = pca.fit_transform(data['current'][data['val']])[0:6]
-
-scan_rate = np.reshape(data['v'][0:6], (-1, 1))
-concentration = np.reshape(data['c'][0:6], (-1, 1))
-
-m = np.append(X_pca_train, scan_rate, axis=1)
-m.shape
-
-train_x = [X_pca_train, scan_rate, concentration]
-
-train_x[X_pca_train
+train_x = np.append(np.append(X_pca_train, scan_rate_train, axis=1), concentration_train, axis=1)
 
 print(train_x)
 
-k0_scale = QuantileTransformer(data['k0'])
-kc_scale = QuantileTransformer(data['kc'])
-d_scale = QuantileTransformer(data['d'])
+#validation data
+
+X_pca_val = pca.transform(data['current'][data['val']])
+
+scan_rate_val = np.reshape(data['v'][data['val']], (-1, 1))
+
+concentration_val = np.reshape(data['c'][data['val']], (-1,1))
+
+val_x = np.append(np.append(X_pca_val, scan_rate_val, axis=1), concentration_val, axis=1)
+
+
+#test data
+
+X_pca_test = pca.transform(data['current'][data['test_ix']])
+
+scan_rate_test = np.reshape(data['v'][data['test_ix']], (-1, 1))
+
+concentration_test = np.reshape(data['c'][data['test_ix']], (-1, 1))
+
+test_x = np.append(np.append(X_pca_test, scan_rate_test, axis= 1), concentration_test, axis = 1)
+
+
+#prediction
+
+scaling = QuantileTransformer(n_quantiles=1000, random_state=0)
+
+
+k0_train = np.reshape(data['k0'][data['train_ix']], (-1, 1))
+kc_train = np.reshape(data['kc'][data['train_ix']], (-1, 1))
+D_train = np.reshape(data['d'][data['train_ix']], (-1, 1))
+
+y= np.array([k0_train,kc_train,D_train])
+
+
+train_y = scaling.fit_transform(D_train)
+
+k0_test = np.reshape(data['k0'][data['test_ix']], (-1, 1))
+kc_test = np.reshape(data['kc'][data['test_ix']], (-1, 1))
+D_test = np.reshape(data['d'][data['test_ix']], (-1, 1))
+
+test_y = scaling.transform(D_test)
+
